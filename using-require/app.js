b5e0023ab7f6f6770a7532d0991c5b38f8c46118
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2018-12-04 10:16:39 +0300

    3-d party modules (11)

--- using-require/app.js
+++ using-require/app.js
@@ -2,12 +2,20 @@ console.log('starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
 const user = os.userInfo();
-
 const res = notes.addNote();
-console.log(res);
-console.log('result:', notes.add(3, 8));
+
+const str = 'string';
+const filteredArray = _.uniq([2,5,5,5,6,7,3,6,8,9,0,4,2,5]);
+
+console.log(_.isString(str));
+
+console.log(filteredArray);
+
+// console.log(res);
+// console.log('result:', notes.add(3, 8));
 
 // console.log(user);
 
