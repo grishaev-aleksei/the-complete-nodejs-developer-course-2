15c79f1f18471ee3e4986ede51d3c2a23338e17b
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-01-21 06:16:39 +0300

    pretty printing objects (29)

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('request');
 const url = 'https://geocode-maps.yandex.ru/1.x/?apikey=2af3249c-1179-4964-9ad3-94ed8449dc34&format=json&geocode=moscow+novosibirskaya+8+23';
 
 request(url, {json: true}, (error, response, body) => {
-    console.log(body)
+    const obj = body.response.GeoObjectCollection.featureMember[0].GeoObject;
+    console.log(JSON.stringify(obj, undefined, 2))
 });
