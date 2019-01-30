3c7a9ae84e8d1f04972ed3ca99ced4d224b04f6a
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-01-30 16:19:39 +0300

    what makes up an HTTP request (30)

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,14 @@ const request = require('request');
 const url = 'https://geocode-maps.yandex.ru/1.x/?apikey=2af3249c-1179-4964-9ad3-94ed8449dc34&format=json&geocode=moscow+novosibirskaya+8+23';
 
 request(url, {json: true}, (error, response, body) => {
-    const obj = body.response.GeoObjectCollection.featureMember[0].GeoObject;
-    console.log(JSON.stringify(obj, undefined, 2))
+    if (error) {
+        console.log(error)
+    } else {
+        const obj = body.response.GeoObjectCollection.featureMember[0].GeoObject;
+        const formattedAddress = obj.metaDataProperty.GeocoderMetaData.Address.formatted;
+        const position = obj.Point;
+        console.log('Address:', formattedAddress);
+        console.log(position);
+        // console.log(JSON.stringify(obj, undefined, 2))
+    }
 });
