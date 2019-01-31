79b2a6211509cb58620993db77de41a2ac09251f
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-01-31 09:11:58 +0300

    encoding user input (31)

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
 
-const url = 'https://geocode-maps.yandex.ru/1.x/?apikey=2af3249c-1179-4964-9ad3-94ed8449dc34&format=json&geocode=moscow+novosibirskaya+8+23';
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'address to fetch weather for in en',
+            string: true
+        }
+    })
+    .help()
+    .alias('h', 'help')
+    .argv;
+
+const apiKey = '2af3249c-1179-4964-9ad3-94ed8449dc34';
+const address = argv.a.replace(/\s/g,'+');
+const url = `https://geocode-maps.yandex.ru/1.x/?apikey=${apiKey}&format=json&geocode=${address}`;
 
 request(url, {json: true}, (error, response, body) => {
     if (error) {
@@ -11,6 +27,5 @@ request(url, {json: true}, (error, response, body) => {
         const position = obj.Point;
         console.log('Address:', formattedAddress);
         console.log(position);
-        // console.log(JSON.stringify(obj, undefined, 2))
     }
 });
