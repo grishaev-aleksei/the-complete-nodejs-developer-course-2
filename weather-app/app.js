875ff02baaecd6ea2b2b8102bc8424c25841bbb9
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-02-01 06:39:47 +0300

    callback errors (32)

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,13 +15,15 @@ const argv = yargs
     .argv;
 
 const apiKey = '2af3249c-1179-4964-9ad3-94ed8449dc34';
-const address = argv.a.replace(/\s/g,'+');
+const address = argv.a.replace(/\s/g, '+');
 const url = `https://geocode-maps.yandex.ru/1.x/?apikey=${apiKey}&format=json&geocode=${address}`;
 
 request(url, {json: true}, (error, response, body) => {
     if (error) {
         console.log(error)
-    } else {
+    } else if (body.response.GeoObjectCollection.featureMember.length === 0) {
+        console.log('not found')
+    } else if (body.response.GeoObjectCollection.featureMember.length > 0) {
         const obj = body.response.GeoObjectCollection.featureMember[0].GeoObject;
         const formattedAddress = obj.metaDataProperty.GeocoderMetaData.Address.formatted;
         const position = obj.Point;
