997e0abf88ed8080a1a0d2466ba19a0e11dd728d
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-02-15 22:28:45 +0300

    Rendering templates with data (43)

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,27 +1,32 @@
 const express = require('express');
+const pug = require('pug');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    // res.send('<h2>Hello Express');
-    res.send({
+    res.render('home', {
         name: 'Aleksei',
         likes: [
             'smoke',
             'drink',
             'sex'
-        ]
+        ],
     })
 });
 
-app.get('/help', (req,res) => {
+app.get('/help', (req, res) => {
     res.sendFile(__dirname + '/public/help.html')
 });
 
 app.get('/about', (req, res) => {
-        res.send('About Page')
+        res.render('about', {
+            title: 'about title',
+            message: 'about body',
+        })
     }
 );
 
