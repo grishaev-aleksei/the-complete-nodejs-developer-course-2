3cefc508a40ae7c49d510d0d342470003664495c
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-02-18 09:58:10 +0300

    express middleware (45)

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const pug = require('pug');
+const fs = require('fs');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile(__dirname + '/server.log', log + '\n', (e) => {
+        if (e) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance')
+// });
+
 
 app.get('/', (req, res) => {
     res.render('home', {
