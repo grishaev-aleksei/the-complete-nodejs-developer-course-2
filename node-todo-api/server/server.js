9e215abf2fe7098147161e0187ebb20ab6ea4bf1
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-03-12 15:04:00 +0300

    Validators Types and defaults (71)

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,28 +6,48 @@ mongoose.connect(url, {useNewUrlParser: true})
     .then(res => console.log('successfully connected'))
     .catch(err => console.log('err', err));
 
-const Todo = mongoose.model('Todo', {
-    text: {
-        type: String
+// const Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 4,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+//
+// new Todo({
+//     text: false
+// }).save()
+//     .then(res => console.log('successfully saved', res))
+//     .catch(err => console.log('there was an error', err));
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
 });
 
-const newToDo = new Todo({
-    text: 'Cook dinner'
-});
+new User({
+    email: '  Mom   '
+}).save()
+    .then(res => {
+        console.log('successfully added user',res);
+        mongoose.disconnect()
+            .then(res => console.log('disconnected'))
+            .catch(err => console.log('error on disconnect'));
+    })
+    .catch(err => console.log('error on adding user',err));
 
-const toDo2 = new Todo({
-    text: 'Make a challenge',
-    completed: true,
-    completedAt: new Date()
-});
 
-toDo2.save()
-    .then(res => console.log('successfully saved', res))
-    .catch(err => console.log('there was an error', err));
+
