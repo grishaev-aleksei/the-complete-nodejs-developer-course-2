94fc4062b07e85c52ecc9d153db2867286b4c4f9
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-03-14 13:51:28 +0300

    Added atlas configuration with heroku env variables

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/Todo-app';
+const url = () =>{
+    if (process.env.ATLAS_DB_LOGIN) {
+        return `mongodb+srv://${process.env.ATLAS_DB_LOGIN}:${process.env.ATLAS_DB_PASS}@cluster0-8jidn.mongodb.net/test?retryWrites=true`
+    }
+    else return 'mongodb://localhost:27017/Todo-app'
+};
 
-mongoose.connect(url, {useNewUrlParser: true})
+
+
+// const url = 'mongodb://localhost:27017/Todo-app';
+//
+// const user = 'dbUser';
+//
+// const pass = encodeURIComponent('LGAa5Qmf5ctltlbf');
+//
+// console.log(pass)
+//
+// const atlasUrl = `mongodb+srv://${user}:${pass}@cluster0-8jidn.mongodb.net/test?retryWrites=true`;
+
+// ATLAS_DB_PASS="LGAa5Qmf5ctltlbf"
+
+mongoose.connect(url(), {useNewUrlParser: true})
     .then(res => console.log('successfully connected'))
     .catch(err => console.log('err', err));
 
