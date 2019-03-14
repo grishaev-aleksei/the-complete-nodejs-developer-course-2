efbfae049f0aced0b9656704c6c2058d4ff21a2f
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-03-14 14:26:25 +0300

    Deploy API to Heroku (80)

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/Todo-app';
+const url = () =>{
+    if (process.env.ATLAS_DB_LOGIN) {
+        return `mongodb+srv://${process.env.ATLAS_DB_LOGIN}:${process.env.ATLAS_DB_PASS}@cluster0-8jidn.mongodb.net/test?retryWrites=true`
+    }
+    else return 'mongodb://localhost:27017/Todo-app'
+};
 
-mongoose.connect(url, {useNewUrlParser: true})
+
+mongoose.connect(url(), {useNewUrlParser: true})
     .then(res => console.log('successfully connected'))
     .catch(err => console.log('err', err));
 
