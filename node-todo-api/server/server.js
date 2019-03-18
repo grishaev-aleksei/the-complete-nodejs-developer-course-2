c51451c0a2406ba1f59a02fd2cc1b51f8fcd2735
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-03-18 14:24:35 +0300

    Getting an Individual resource (78)

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {todo} = require('./models/todo');
-const {User} = require('./models/user');
+const {user} = require('./models/user');
 
 const app = express();
 
@@ -28,6 +29,44 @@ app.get('/todos', (req, res) => {
             res.send({todos})
         })
         .catch(err => res.status(400).send(err))
+});
+
+app.get('/todos/:id', (req, res) => {
+    const todoId = req.params.id;
+    if (!ObjectID.isValid(todoId)) {
+        return res.status(400).json({error: 'invalid ID'})
+    }
+    todo.findById(todoId)
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).json({error: 'user not found'})
+            }
+            res.status(200).json(todo)
+        })
+        .catch(err => res.status(400).json({error: err}))
+});
+
+app.get('/users', (req, res) => {
+    user.find()
+        .then(users => {
+            res.send({users})
+        })
+        .catch(err => res.status(400).json({error: err}));
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).json({error: 'invalid ID'})
+    }
+    user.findById(id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({error: 'user not found'})
+            }
+            res.status(200).json(user)
+        })
+        .catch(err => res.status(400).json({error: err}))
 });
 
 app.listen(3000, () => {
