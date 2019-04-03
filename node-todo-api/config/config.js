4a422a155e065ee267b5578eedf0646b5d99132b
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-04-03 10:18:26 +0300

    Improving App Configuration (101)

--- node-todo-api/config/config.js
+++ node-todo-api/config/config.js
@@ -15,15 +15,3 @@ if (env === 'development' || env === 'test') {
         process.env[key] = envConfig[key]
     });
 }
-
-
-// if (env() === "development") {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/Todo-app';
-// } else if (env() === 'test') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/Todo-app-test';
-// } else if (env() === 'production') {
-//     process.env.MOBGODB_URI =
-//         `mongodb+srv://dbUser:LGAa5Qmf5ctltlbf@cluster0-8jidn.mongodb.net/test?retryWrites=true`;
-// }
