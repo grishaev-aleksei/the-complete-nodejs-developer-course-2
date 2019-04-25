0169f763fcd1b9d5442a2ebc33d29e4c2032626f
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-04-25 09:26:33 +0300

    Async await basics (130)

--- async-await/app-promises.js
+++ async-await/app-promises.js
@@ -56,16 +56,30 @@ const getStatus = (userId) => {
 
                 })
         })
+};
+
+const getStatusAlt = async (userId) => {
+    const user = await getUser(userId);
+    const grades = await getGrades(user.schoolId);
+    let average = 0;
+    if (grades.length > 0) {
+        average = grades.map((grade) => grade.grade)
+            .reduce((a, b) => a + b) / grades.length;
+    }
+    return `${user.name} has a ${average}% in the class`
 
 };
 
-getUser(2)
-    .then(user => console.log(user))
-    .catch(err => console.log(err));
+getStatusAlt(1).then(res => console.log(res))
+    .catch(err => console.log('err:', err.message));
 
-getGrades(101)
-    .then(grades => console.log(grades))
-    .catch(err => console.log(err));
-getStatus(1)
-    .then(status => console.log(status))
-    .catch(err => console.log(err));
+// getUser(2)
+//     .then(user => console.log(user))
+//     .catch(err => console.log(err));
+//
+// getGrades(101)
+//     .then(grades => console.log(grades))
+//     .catch(err => console.log(err));
+// getStatus(1)
+//     .then(status => console.log(status))
+//     .catch(err => console.log(err));
